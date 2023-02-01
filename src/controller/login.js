1190b41ff1296b0e1be53010ebac9f03b2229d09
BornSava9e <Parasanand1999@gmail.com>
2023-02-01 18:27:17 +0530

    kuch bhi

--- src/controller/login.js
+++ src/controller/login.js
@@ -1,3 +1,5 @@
+const userModel = require("../model/userModel")
+
 router.post("/create",async function(req,res){
     let data = req.body
     let save = await usenModel.create(data)
@@ -5,18 +7,25 @@ router.post("/create",async function(req,res){
 })
 
 router.post("/login", async function(req,res){
+    try{
     const {email,password} = req.body
 
-    if(!email) return res.status(400).send({status:false, message: "email is required"})
+    if(!email) return res.status(400).send({status:false, message: "email is required"}) 
+    let check = await userModel.findOne({email : email})  // for getting the hashed password from db
+    let hashedToken = check.password        // assign hashed token into hashedToken
+
     if(!password) return res.status(400).send({status:false, message: "password is required"})
 
-    const exist = await userModel.findOne({email : email, passowrd : password})
+    let decrypt = await bcrypt.compare(password,hashedToken)  // have boolean true or false 
 
-    if (!check){
-        return res.status(404).send({status : false, message : "email or password is incorrect"})
-
+    if(decrypt === true){
+        let token = jwt.sign({userId : check._id.toString()}, "dummykey",{expiresIn : "1h"})
+        return res.status(200).send({status : true, message : "User login successful",data : {userId : check._id, token : token}})
     }else{
-        let token = jwt.sign({userId : exist._id.toString()}, "dummykey",{expiresIn : "1h"})
-        return res.status(200).send({status : true, message : "User login successful",data : {userId : exist._id, token : token}})
+        return res.status(400).send( {status : false, message : "enter valid password"})
     }
+    }catch(err){
+        res.status(500).send({status : false, error : err.message})
+    }
+
 })
