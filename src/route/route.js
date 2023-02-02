e68fc06b7a70ac0ab9bc1085fe3f4c69fb1e0edf
bhavi321 <bhavialkhaniya5@gmail.com>
2023-02-02 15:54:13 +0530

    nashe wala commit

--- src/route/route.js
+++ src/route/route.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const {registerUser,loginUser} = require("../controller/userController")
-
+const {registerUser,loginUser,getUserByParams} = require("../controller/userController")
+const {authentication} = require("../middlewares/auth")
 
 
 router.post("/register",registerUser)
 router.post("/login",loginUser)
+router.get("/user/:userId/profile",authentication,getUserByParams)
 
 module.exports = router
