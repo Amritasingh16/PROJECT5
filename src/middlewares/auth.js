241e45b4b34dc8d9b3461c6364c94cb4db4479d2
BornSava9e <Parasanand1999@gmail.com>
2023-02-03 12:39:12 +0530

    ss

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,9 +1,7 @@
 const jwt = require("jsonwebtoken")
 const authentication = async function(req,res,next){
     try{
-        let bearerToken  = req.header("Authorization")
-    
-       
+        let bearerToken  = req.header("Authorization") 
         if(!bearerToken) return res.status(400).send({status : false, message : "bearerToken is mandatory"})
         bearerToken = bearerToken.replace("Bearer ","")
         jwt.verify(bearerToken,"dummykey",function(err,decode){
