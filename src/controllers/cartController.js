be6eabcb9edbda8a08f81a347f164a9e8497838a
sourav4125 <souravmisra4125@gmail.com>
2023-02-06 14:56:47 +0530

    del api

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -37,7 +37,27 @@ const getCartByParams = async function(req,res){
 
 const deleteCartByParams = async function(req,res){
 
+    try {
+        let userId=req.params.userId;
+        let verifyToken= req.bearerToken;
 
+        if (!mongoose.isValidObjectId(userId)) return res.status(400).send({ status: false, message: "userId is invalid" })
+        if(userId!==verifyToken) return res.status(403).send({status:false,message:"You are not authorised"})
+       
+        let usercheck=await userModel.findOne({_id:userId})
+        if(!usercheck) return res.status(404).send({status:false,message:"User not Found"})
+
+        let cartDelete=await cartModel.findOneAndUpdate({userId:userId},{$set:{items:[],totalItems:0,totalPrice:0}},{new:true})
+        if(!cartDelete) return res.status(404).send({status:false,message:"Cart Doesn't Exist"})
+       
+        return res.status(204).send() // response struture
+       
+
+    } catch (error) {
+        return res.status(500).send({status: false,error:error.message})
+    }
+    
+   
 
 
 }
